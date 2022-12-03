f972916e4e943502dc7682903a70ceba50cb9725
Krish Baisoya <krishbaisoya.it2020@gmail.com>
2022-12-03 17:42:40 +0530

    Added vocab; Created collate_fn for padding

--- data.py
+++ data.py
@@ -4,6 +4,8 @@ Written by KrishPro @ KP
 filename: `data.py`
 """
 
+from typing import List
+
 import torch.nn as nn
 import torch.utils.data as data
 import torchaudio.transforms as T
@@ -12,10 +14,27 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import random
+import string
 import torch
 import os
 
 DATA_DIR = "../../Datasets/CommonVoice/"
+
+class Vocab:
+    chars: List[str] = ["[PAD]", " ", "'"] + list(string.ascii_lowercase)
+
+    char_to_int = {c:i for i, c in enumerate(chars)}
+    int_to_char = {i:c for i, c in enumerate(chars)}
+
+    @staticmethod
+    def text_to_ints(text: str):
+        ints = [Vocab.char_to_int[char] for char in text.lower() if char in Vocab.char_to_int]
+        return ints
+
+    @staticmethod
+    def ints_to_text(ints: List[int]):
+        text = ''.join([Vocab.int_to_char[int] for int in ints if int in Vocab.int_to_char])
+        return text
 
 
 class Lambda(nn.Module):
@@ -36,6 +55,7 @@ class Dataset(data.Dataset):
         self.walker = pd.read_csv(csv_path)[['filename', 'text']]
         self.images_dir = images_dir
         self.sample_rate = sample_rate
+        self.vocab = Vocab()
 
         self.spec_aug = nn.Sequential(
             T.FrequencyMasking(freq_mask_param=freq_mask),
@@ -54,14 +74,29 @@ class Dataset(data.Dataset):
     def __getitem__(self, idx):
         filename, text = self.walker.iloc[idx]
 
-        audio, _ = io.load(os.path.join(self.images_dir, filename))     
+        audio, _ = io.load(os.path.join(self.images_dir, filename))  
 
-        audio = self.audio_transformers(audio)
+        audio: torch.Tensor = self.audio_transformers(audio)
 
-        return audio, text
+        if audio.size(2) > 1024:
+            return self[idx-1] if idx != 0 else self[idx+1]   
+
+        text = self.vocab.text_to_ints(text)
+
+        return audio.squeeze(0).T, text
 
     def __len__(self):
         return len(self.walker)
+
+    @staticmethod
+    def collate_fn(data):
+        audios, texts = zip(*data)
+
+        audios = nn.utils.rnn.pad_sequence(audios, batch_first=True).transpose(1, 2)
+
+        texts = nn.utils.rnn.pad_sequence(map(torch.tensor, texts), batch_first=True)
+
+        return audios, texts
 
 if __name__ == '__main__':
     dataset = Dataset(os.path.join(DATA_DIR, 'cv-valid-train'), os.path.join(DATA_DIR, 'cv-valid-train.csv'))
